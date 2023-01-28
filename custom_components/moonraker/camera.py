d972daf5d00387034527405d8129d63536206784
Marc-Olivier Arsenault <marcolivier.arsenault@gmail.com>
2023-01-28 23:16:15 +0000

    add images

---
+++ custom_components/moonraker/camera.py
@@ -0,0 +1,40 @@
+"""Support for OctoPrint binary camera."""
+from __future__ import annotations
+
+from homeassistant.components.mjpeg.camera import MjpegCamera
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from .const import DOMAIN, NAME, CONF_URL
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up the available Moonraker camera."""
+    coordinator = hass.data[DOMAIN][config_entry.entry_id]
+
+    async_add_entities([MoonrakerCamera(config_entry, coordinator)])
+
+
+class MoonrakerCamera(MjpegCamera):
+    """Representation of an Moonraker Camera Stream."""
+
+    def __init__(self, config_entry, coordinator) -> None:
+        """Initialize as a subclass of MjpegCamera."""
+
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, config_entry.entry_id)}
+        )
+        self.url = coordinator.config_entry.data.get(CONF_URL)
+        super().__init__(
+            device_info=self._attr_device_info,
+            mjpeg_url=f"{self.url}/webcam/?action=stream",
+            name="Moonraker Camera",
+            still_image_url=f"{self.url}/webcam/?action=snapshot",
+            unique_id=f"{NAME}_camera",
+        )
