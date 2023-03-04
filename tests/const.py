edf07ac90075b378d623ecd76cfef9c8fac81d73
Eric Tremblay <etremblay@distech-controls.com>
2023-03-03 23:15:50 -0500

    Add moonraker api key support
    The field can be empty or need to have an alphanumerical string of 32 characters
    the key is pass to moonraker-api

--- tests/const.py
+++ tests/const.py
@@ -1,5 +1,5 @@
 """Constants for integration_blueprint tests."""
-from custom_components.moonraker.const import CONF_PORT, CONF_URL
+from custom_components.moonraker.const import CONF_API_KEY, CONF_PORT, CONF_URL
 
 # Mock config data to be used across multiple tests
-MOCK_CONFIG = {CONF_URL: "1.2.3.4", CONF_PORT: "1234"}
+MOCK_CONFIG = {CONF_URL: "1.2.3.4", CONF_PORT: "1234", CONF_API_KEY: ""}
