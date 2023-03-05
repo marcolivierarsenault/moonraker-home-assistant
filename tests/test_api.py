4f7a2deff6742e73c91fa65f970bc9ce1b6fe150
Marc-Olivier Arsenault <marcolivier.arsenault@gmail.com>
2023-03-05 23:57:16 +0000

    manage None port from pre 0.3.0

--- tests/test_api.py
+++ tests/test_api.py
@@ -15,3 +15,16 @@ async def test_connect_client():
         assert moonraker_api.running
         await moonraker_api.stop()
         assert not moonraker_api.running
+
+
+async def test_none_port_connect_client():
+    """Test connect client"""
+    with patch("moonraker_api.MoonrakerClient"), patch(
+        "moonraker_api.websockets.websocketclient.WebsocketClient.connect"
+    ), patch("moonraker_api.websockets.websocketclient.WebsocketClient.disconnect"):
+        moonraker_api = MoonrakerApiClient("notaURL", None, port=None, api_key="1dd2")
+        assert not moonraker_api.running
+        await moonraker_api.start()
+        assert moonraker_api.running
+        await moonraker_api.stop()
+        assert not moonraker_api.running
