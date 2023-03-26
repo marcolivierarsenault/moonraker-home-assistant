de98aa3dc7a81eb64f0cdf2973927f28a9c7d6e3
Marc-Olivier Arsenault <marcolivier.arsenault@gmail.com>
2023-03-26 01:47:05 +0000

    change entry setup to async

--- custom_components/moonraker/__init__.py
+++ custom_components/moonraker/__init__.py
@@ -75,9 +75,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     for platform in PLATFORMS:
         if entry.options.get(platform, True):
             coordinator.platforms.append(platform)
-            hass.async_add_job(
-                hass.config_entries.async_forward_entry_setup(entry, platform)
-            )
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     entry.async_on_unload(entry.add_update_listener(async_reload_entry))
     return True
 
