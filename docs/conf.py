eefc61d6b5c59137065dc16834c05d747e6e45f6
Marc-Olivier Arsenault <marcolivier.arsenault@gmail.com>
2023-04-07 15:10:15 +0000

    add missing extension

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ author = "Marc-Olivier Arsenault, Eric Tremblay"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autosectionlabel"]
+extensions = ["sphinx.ext.autosectionlabel", "sphinx_rtd_theme"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
