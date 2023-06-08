a692ed8e557b19765fc94bafcb858e6638049c8a
Marc-Olivier Arsenault <marcolivier.arsenault@gmail.com>
2023-06-08 09:11:05 -0400

    change doc theme to furo (#142)

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ author = "Marc-Olivier Arsenault, Eric Tremblay"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autosectionlabel", "sphinx_rtd_theme"]
+extensions = ["sphinx.ext.autosectionlabel"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
@@ -22,5 +22,5 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 html_static_path = ["_static"]
