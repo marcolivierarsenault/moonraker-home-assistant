50298312f9d7c908c5852655ff0206bbc8c36141
Marc-Olivier Arsenault <marcolivier.arsenault@gmail.com>
2023-07-13 00:06:02 -0400

    Update const.py

--- custom_components/moonraker/const.py
+++ custom_components/moonraker/const.py
@@ -6,7 +6,7 @@ from homeassistant.const import Platform
 # Base component constants
 DOMAIN = "moonraker"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "0.9.1"
+VERSION = "0.9.2"
 MANIFACTURER = "@marcolivierarsenault"
 
 # Platforms
