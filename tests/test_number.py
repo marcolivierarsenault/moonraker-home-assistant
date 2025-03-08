721ae2f31605a0778d124c2f93dbdd45c9b21602
Marc-Olivier Arsenault <marcolivier.arsenault@gmail.com>
2025-03-08 13:11:11 +0000

    add update test

--- tests/test_number.py
+++ tests/test_number.py
@@ -62,3 +62,65 @@ async def test_number_set_value(hass, number, get_default_api_response):
             METHODS.PRINTER_GCODE_SCRIPT.value,
             script=f"SET_PIN PIN={number.split('_')[3]} VALUE=0.5",
         )
+
+
+async def test_set_target(hass, get_default_api_response):
+    """Test."""
+    config_entry = MockConfigEntry(domain=DOMAIN, data=MOCK_CONFIG, entry_id="test")
+    config_entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+
+    with patch(
+        "moonraker_api.MoonrakerClient.call_method",
+        return_value={**get_default_api_response},
+    ) as mock_api:
+        await hass.services.async_call(
+            NUMBER_DOMAIN,
+            SERVICE_SET_VALUE,
+            {
+                ATTR_ENTITY_ID: "number.mainsail_extruder_target",
+                "value": 50,
+            },
+            blocking=True,
+        )
+
+        await hass.async_block_till_done()
+        mock_api.assert_called_once_with(
+            METHODS.PRINTER_GCODE_SCRIPT.value,
+            script="M104 T0 S50.0",
+        )
+
+        mock_api.reset_mock()
+        await hass.services.async_call(
+            NUMBER_DOMAIN,
+            SERVICE_SET_VALUE,
+            {
+                ATTR_ENTITY_ID: "number.mainsail_extruder1_target",
+                "value": 60,
+            },
+            blocking=True,
+        )
+
+        await hass.async_block_till_done()
+        mock_api.assert_called_once_with(
+            METHODS.PRINTER_GCODE_SCRIPT.value,
+            script="M104 T1 S60.0",
+        )
+
+        mock_api.reset_mock()
+        await hass.services.async_call(
+            NUMBER_DOMAIN,
+            SERVICE_SET_VALUE,
+            {
+                ATTR_ENTITY_ID: "number.mainsail_bed_target",
+                "value": 70,
+            },
+            blocking=True,
+        )
+
+        await hass.async_block_till_done()
+        mock_api.assert_called_once_with(
+            METHODS.PRINTER_GCODE_SCRIPT.value,
+            script="M140 S70.0",
+        )
